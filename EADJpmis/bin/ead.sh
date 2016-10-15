6b76d3e8456f25f6ee0ff6c05c31b93650434399
Krish Dey <krishdey@Krishs-MacBook-Pro.local>
2016-10-15 18:39:35 -0400

    EAD

--- EADJpmis/bin/ead.sh
+++ EADJpmis/bin/ead.sh
@@ -29,7 +29,7 @@ then
     EAD_ACTION=$2
 else
     # Printing usage information
-    echo "Usage: apacheds.sh [<instance name>] <action>"
+    echo "Usage: ead.sh [<instance name>] <action>"
     echo "If <instance name> is ommited, 'default' will be used."
     echo "<action> is one of start, stop."
     exit 1
@@ -51,8 +51,8 @@ CLASSPATH=$(JARS=("$EAD_HOME"/lib/*.jar); IFS=:; echo "${JARS[*]}")
 
 EAD_INSTANCE="$EAD_INSTANCES/$EAD_INSTANCE_NAME"
 
-EAD_OUT="$EAD_INSTANCE/log/apacheds.out"
-EAD_PID="$EAD_INSTANCE/run/apacheds.pid"
+EAD_OUT="$EAD_HOME/log/ead.out"
+EAD_PID="$EAD_HOME/run/ead.pid"
 touch $EAD_OUT
 touch $EAD_PID
 
@@ -64,20 +64,20 @@ fi
 
 if [ "$EAD_ACTION" = "start" ]; then
     # Printing instance information
-    [ $HAVE_TTY -eq 1 ] && echo "Starting ApacheDS instance '$EAD_INSTANCE_NAME'..."
+    [ $HAVE_TTY -eq 1 ] && echo "Starting EAD instance '$EAD_INSTANCE_NAME'..."
 
     if [ -f $EAD_PID ]; then
         PID=`cat $EAD_PID`
         if kill -0 $PID > /dev/null 2>&1; then
-            echo "ApacheDS is already running as $PID"
+            echo "EAD Server is already running as $PID"
             exit 0
         fi
     fi
 
-    # Launching ApacheDS
+    # Launching EAD Server
      eval "\"$RUN_JAVA\"" \
-     -Dlog4j.configuration="\"file:$EAD_INSTANCE/conf/log4j.properties\"" \
-     -Dapacheds.log.dir="\"$EAD_INSTANCE/log\"" \
+     -Dlog4j.configuration="\"file:$EAD_HOME/conf/log4j.properties\"" \
+     -Dapacheds.log.dir="\"$EAD_HOME/log\"" \
      -Dead.server.port=10389 \
      -Dhadoop-group-mapping=/Hadoop/hadoop-group-mapping.xml \
      -classpath "\"$CLASSPATH\"" \
@@ -87,7 +87,7 @@ if [ "$EAD_ACTION" = "start" ]; then
 
 elif [ "$EAD_ACTION" = "run" ]; then
     # Printing instance information
-    [ $HAVE_TTY -eq 1 ] && echo "Running ApacheDS instance '$EAD_INSTANCE_NAME'..."
+    [ $HAVE_TTY -eq 1 ] && echo "Running EAD instance '$EAD_INSTANCE_NAME'..."
       eval "\"$RUN_JAVA\"" \
      -Dlog4j.configuration="\"file:$EAD_HOME/conf/log4j.properties\"" \
      -Dapacheds.log.dir="\"$EAD_HOME/log\"" \
@@ -100,18 +100,18 @@ elif [ "$EAD_ACTION" = "status" ]; then
     if [ -f $EAD_PID ]; then
         PID=`cat $EAD_PID`
         if kill -0 $PID > /dev/null 2>&1; then
-            echo "ApacheDS is running as $PID"
+            echo "EAD is running as $PID"
         else
-            echo "ApacheDS is not running"
+            echo "EAD is not running"
         fi
     else
-        [ $HAVE_TTY -eq 1 ] && echo "ApacheDS is not running"
+        [ $HAVE_TTY -eq 1 ] && echo "EAD is not running"
     fi
 elif [ "$EAD_ACTION" = "stop" ]; then
     # Printing instance information
     if [ -f $EAD_PID ]; then
         PID=`cat $EAD_PID`
-        [ $HAVE_TTY -eq 1 ] && echo "Stopping ApacheDS instance '$EAD_INSTANCE_NAME' running as $PID"
+        [ $HAVE_TTY -eq 1 ] && echo "Stopping EAD Servr instance '$EAD_INSTANCE_NAME' running as $PID"
 
         kill -15 $PID > /dev/null 2>&1
 
@@ -120,13 +120,13 @@ elif [ "$EAD_ACTION" = "stop" ]; then
             kill -0 $PID > /dev/null 2>&1 -gt 0
             if [ $? > 0 ]; then
                 rm -f $EAD_PID > /dev/null 2>&1
-                [ $HAVE_TTY -eq 1 ] && echo "ApacheDS instance '$EAD_INSTANCE_NAME' stopped successfully"
+                [ $HAVE_TTY -eq 1 ] && echo "EAD Servr instance '$EAD_INSTANCE_NAME' stopped successfully"
                 break
             fi
             sleep 1
             ATTEMPTS_REMAINING=`expr $ATTEMPTS_REMAINING - 1`
         done
     else
-        [ $HAVE_TTY -eq 1 ] && echo "ApacheDS is not running, $EAD_PID does not exist"
+        [ $HAVE_TTY -eq 1 ] && echo "EAD Servr is not running, $EAD_PID does not exist"
     fi
 fi
