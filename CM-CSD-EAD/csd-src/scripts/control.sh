bd9b9381e22253a98e70df7823d90e08d19fdf50
Krish Dey <krishnendu.dey@gmail.com>
2016-10-21 18:21:46 -0400

    Update control.sh

--- CM-CSD-EAD/csd-src/scripts/control.sh
+++ CM-CSD-EAD/csd-src/scripts/control.sh
@@ -2,16 +2,19 @@
 set -x
 CMD=$1
 
-_term() { 
-  echo "Caught SIGTERM signal!" 
-  kill -TERM "$child" 2>/dev/null
-}
-
-trap _term SIGTERM
 
 RETVAL=0
 PROG="ead"
 NAME="ead"
+
+echo "CONF_DIR $CONF"
+echo "EAD_HEAP_SIZE: ${EAD_HEAP_SIZE}"
+echo "EAD_JAVA_OPTS: ${EAD_JAVA_OPTS}"
+echo "EAD_PORT: ${EAD_PORT}"
+
+
+
+export EAD_JAVA_OPTS="$CSD_JAVA_OPTS -Xmx${EAD_HEAP_SIZE}M ${EAD_JAVA_OPTS}"
 
 if [ -z $EAD_HOME ] 
 	then
@@ -25,6 +28,9 @@ adjust_config() {
    echo "Adjusting program EAD.."
 }
 
+export EAD_XML_PATH="$CONF_DIR/hadoop_group_mapping.xml"
+
+
 echo "The command is $CMD"
 case $CMD in
   (deploy)
@@ -35,16 +41,8 @@ case $CMD in
    
    echo "Starting $PROG on" `hostname`
    rm -rf  $EAD_HOME/run/*
-   exec $EAD_HOME/bin/ead.sh krish run &
-   child=$!
-   wait "$child"
+   exec $EAD_HOME/bin/ead.sh krish run 
    ;;
- 
- (stop)
-  
-    echo "Shutting down $PROG on" `hostname`
-    pkill -P `pgrep -f "ead.sh krish run"`
-    ;;
   (*)
     echo "Don't understand [$CMD]"
     ;;
